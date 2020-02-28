67321cc5dac3f35664e1e9632e8edc4b8b20af89
WarOfDevil <vnktux@protonmail.com>
2020-02-28 23:05:28 +0100

    Add administrator menu
    Add administrator menu to edit page elements and users. Updated favicon, default user avatar and logo.

--- InstaFood/InstaFood.DataAccess/ApplicationDbContext.cs
+++ InstaFood/InstaFood.DataAccess/ApplicationDbContext.cs
@@ -1,13 +1,30 @@
-﻿using InstaFood.Models;
+﻿/*
+    Description: ApplicationDbContext class
+    
+    Author: WarOfDevil          Date: 27-02-2020
+*/
+
+using InstaFood.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace InstaFood.DataAccess
 {
+	/// <summary>
+	/// ApplicationDbContext class
+	/// Initialize EF database, inherit settings for user Identity
+	/// </summary>
 	public class ApplicationDbContext : IdentityDbContext
 	{
+		/// <summary>
+		/// Category table
+		/// </summary>
 		public DbSet<Category> Category {get; set;}
 
+		/// <summary>
+		/// Default costructor
+		/// </summary>
+		/// <param name="options">Database connection options</param>
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 			: base(options)
 		{
